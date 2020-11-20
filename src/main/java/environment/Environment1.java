d6daafedddad3e8630df6b9f15c38320fee3bfc6
Tuteiko, Liudmyla [External] <Liudmyla.Tuteiko@externals.adidas.com>
2020-11-20 12:05:33 +0200

    Refactor env - make static

--- src/main/java/environment/Environment1.java
+++ src/main/java/environment/Environment1.java
@@ -3,11 +3,12 @@ package environment;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
-public class Environment {
-    private static String environmentPath = "src/test/resources/environment.properties";
-    private static PropertiesConfiguration applicationProps;
+public class Environment1 {
+    private String environmentPath;
+    private PropertiesConfiguration applicationProps;
 
-    static {
+    public Environment1(String environmentPath) {
+        this.environmentPath = environmentPath;
         try {
             applicationProps = new PropertiesConfiguration(environmentPath);
         } catch (ConfigurationException e) {
@@ -15,11 +16,11 @@ public class Environment {
         }
     }
 
-    public static String getEnvProperty(String key) {
+    public String getEnvProperty(String key) {
         return applicationProps.getProperty(key).toString();
     }
 
-    public static void setEnvProperty(String key, String value) {
+    public void setEnvProperty(String key, String value) {
         applicationProps.setProperty(key, value);
         try {
             applicationProps.save();
