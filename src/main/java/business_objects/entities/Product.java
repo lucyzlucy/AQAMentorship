1b4f43f1aba89014b673fd8c4cac5dfb27d7a3cf
Tuteiko, Liudmyla [External] <Liudmyla.Tuteiko@externals.adidas.com>
2020-12-01 10:51:03 +0200

    "Added equals and hashcode methods to Product. Added total sum and quantity"

--- src/main/java/business_objects/entities/Product.java
+++ src/main/java/business_objects/entities/Product.java
@@ -3,10 +3,37 @@ package business_objects.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 public class Product {
     private String name;
-    private String price;
+    private double price;
     private int quantity = 1;
+    private double totalPrice;
+
+    public void setQuantity(int q){
+        this.quantity = q;
+        totalPrice = price * quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (o == this) return true;
+        if (!(o instanceof Product)) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) &&
+                price == product.price &&
+                quantity == product.quantity &&
+                totalPrice == product.totalPrice;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, quantity, totalPrice);
+    }
 }
